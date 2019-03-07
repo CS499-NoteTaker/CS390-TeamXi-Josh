01fd3fa167e7ed46ebf6de8de9141e50d78bc2a9
U-PLU\gjukahn <gjukahn@CSCI18986.cs.PLU.EDU>
2019-03-06 19:01:44 -0800

    Added code to Board and BoardTest classes

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,2 +1,22 @@
+//Initializes and stores all the gameboard Points in a 2-D array.
 public class Board {
+    private int boardSize = 19;
+    private int[][] board;
+
+    /*
+    * Creates a new Board Object which instantiates a 2-D array of Point objects.
+    */
+    public Board(){
+        board = new int[boardSize][boardSize];
+        for(int i = 0; i < boardSize; i++){
+            for(int j = 0; j < boardSize; j++){
+                Point p = new Point(i, j);
+                board[i][j] = p;
+            }
+        }
+    }
+
+    public Point getPointAtLocation(int x, int y){
+        return board[x - 1][y - 1];
+    }
 }
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,2 +1,30 @@
+import org.junit.*;
+
 public class BoardTest {
+    private Point p1;
+    private Point p2;
+    private Point p3;
+    private Board b;
+
+    @Before
+    public void setUp() {
+        p1 = new Point(0, 0);
+        p2 = new Point(17, 17);
+        p3 = new Point(12, 7);
+        board = new Board();
+    }
+
+    @Test
+    public void testBoard(){
+        Assert.assertTrue(comparePoints(board.getPointAtLocation(0, 0)));
+    }
+
+
+
+
+    public boolean comparePoints(Point p1, point p2){
+        return (p1.getX() == p2.getX()) && (p1.getY() == p2.getY());
+    }
+
 }
+
