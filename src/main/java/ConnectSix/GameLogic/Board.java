39f7e136441066cb3f55a6f64d3797529f2f1819
Kaitlyn Gold <Goldkr@plu.edu>
2019-03-06 19:27:40 -0800

    fixes towards Board tests

--- src/main/java/ConnectSix/GameLogic/Board.java
+++ src/main/java/ConnectSix/GameLogic/Board.java
@@ -3,13 +3,13 @@ package ConnectSix.GameLogic;
 //Initializes and stores all the gameboard Points in a 2-D array.
 public class Board {
     private int boardSize = 19;
-    private int[][] board;
+    private Point[][] board;
 
     /*
      * Creates a new Board Object which instantiates a 2-D array of Point objects.
      */
     public Board(){
-        board = new int[boardSize][boardSize];
+        board = new Point[boardSize][boardSize];
         for(int i = 0; i < boardSize; i++){
             for(int j = 0; j < boardSize; j++){
                 Point p = new Point(i, j);
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,10 +1,12 @@
 import org.junit.*;
+import ConnectSix.GameLogic.Board;
+import ConnectSix.GameLogic.Point;
 
 public class BoardTest {
     private Point p1;
     private Point p2;
     private Point p3;
-    private Board b;
+    private Board board;
 
     @Before
     public void setUp() {
@@ -16,16 +18,16 @@ public class BoardTest {
 
     @Test
     public void testPointOnBoard1(){
-        Assert.assertTrue(comparePoints(board.getPointAtLocation(0, 0)), p1);
+        Assert.assertTrue(comparePoints(board.getPointAtLocation(0, 0), p1));
     }
 
     @Test
     public void testPointOnBoard2(){
-        Assert.assertTrue(comparePoints(board.getPointAtLocation(17, 17)), p2);
+        Assert.assertTrue(comparePoints(board.getPointAtLocation(17, 17), p2 ));
     }
     
 
-    public boolean comparePoints(Point p1, point p2){
+    public boolean comparePoints(Point p1, Point p2){
         return (p1.getX() == p2.getX()) && (p1.getY() == p2.getY());
     }
 
