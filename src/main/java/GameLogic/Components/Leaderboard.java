b0a184fbebb52c6a32408352d7b126acad5ca71c
U-PLU\gjukahn <gjukahn@CSCI18988.cs.PLU.EDU>
2019-03-27 19:08:22 -0700

    Added a LeaderBoard and LeaderboardTest Class

---
+++ src/main/java/GameLogic/Components/Leaderboard.java
@@ -0,0 +1,41 @@
+package GameLogic.Components;
+
+import UserData.User;
+
+import java.util.ArrayList;
+
+public class Leaderboard {
+    private ArrayList<User> leaderboard;
+
+    public Leaderboard(ArrayList<User> users){
+        leaderboard = sortUsersByWinLossRatioHighestToLowest(users);
+    }
+
+    public ArrayList<User> getLeaderboard(){
+        return leaderboard;
+    }
+
+    private ArrayList<User> sortUsersByWinLossRatioHighestToLowest(ArrayList<User> users){
+        ArrayList<User> usersSorted = new ArrayList<User>();
+        for(int i = 0; i < users.size(); i++){
+            usersSorted = putUserInRightPlace(users.get(i), usersSorted);
+        }
+        return usersSorted;
+    }
+
+    /**
+     * Puts a single user in the right part of the "sorted" userList based on W/L ratio
+     * @return The ArrayList, with the user added to it
+     */
+    private ArrayList<User> putUserInRightPlace(User user, ArrayList<User> usersSorted){
+        //TODO: Perhaps add a check method to see if the user is already in the usersSorted ArrayList?
+        boolean userHasBeenInserted = false;
+        for(int i = 0; i < usersSorted.size(); i++) {
+            if(!userHasBeenInserted && user.getWinLossRatio() >= usersSorted.get(i).getWinLossRatio()){
+                usersSorted.add(i, user);
+                userHasBeenInserted = true;
+            }
+        }
+        return usersSorted;
+    }
+}
---
+++ src/test/java/LeaderBoardTest.java
@@ -0,0 +1,27 @@
+import GameLogic.Components.Point;
+import UserData.User;
+import UserData.UserList;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LeaderBoardTest {
+
+    @Before
+    public void setUp(){
+        User joe = new User("Joe");  //0.0
+        joe.addLoss();
+        User adam = new User("Adam"); //1.0
+        adam.addLoss();
+        adam.addWin();
+        User chad = new User("Chad"); //2.0
+        chad.addWin();
+        chad.addWin();
+        chad.addLoss();
+    }
+
+//    @Test
+//    public void testLeaderBoard(){
+//
+//    }
+}
