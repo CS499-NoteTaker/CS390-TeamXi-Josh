aaf1fef149075b5b52b112400a328acccbe907fe
U-PLU\gjukahn <gjukahn@CSCI18988.cs.PLU.EDU>
2019-04-01 12:25:52 -0700

    Stubbed out Move class, it was completely empty before.

--- src/main/java/GameLogic/Components/Move.java
+++ src/main/java/GameLogic/Components/Move.java
@@ -1,4 +1,19 @@
 package GameLogic.Components;
 
+import UserData.User;
+
+import java.util.Date;
+
 public class Move {
+    private Date date;
+    private User user;
+    Point point;
+
+    public Move(){
+
+    }
+
+    public String toString(){
+        return null;
+    }
 }
