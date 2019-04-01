5f3a437193f7075ea24d9fc5b39b45544a348968
Joshua <moranja@plu.edu>
2019-04-01 12:51:21 -0700

    Merge branch 'UserAndUserList_3-30' into testMergeMaster
    # Conflicts:
    #	src/main/java/GameLogic/Components/Game.java
    #	src/main/java/GameLogic/Components/Move.java
    #	src/main/java/GameLogic/Components/MoveList.java
    #	src/main/java/UserData/UserList.java

--- src/main/java/GameLogic/Components/Move.java
+++ src/main/java/GameLogic/Components/Move.java
@@ -2,18 +2,33 @@ package GameLogic.Components;
 
 import UserData.User;
 
+import java.sql.Time;
 import java.util.Date;
 
 public class Move {
-    private Date date;
-    private User user;
-    Point point;
+    private User user;      // User that made the move
+    private Date date;      // Date and time that user made move.
+    private Point point;    // Point that user made a move on.
 
-    public Move(){
-
+    // Constructor
+    public Move (User u, Point p) {
+        this.user = u;
+        this.point = p;
+        this.date = new Date(); // Grabs today's date.
     }
 
-    public String toString(){
-        return null;
+    /**
+     * Gets the the user name of the person who made
+     * this move.
+     * @return String username
+     */
+    public String getWhoMoved() {
+        return this.user.toString();
+    }
+    @Override
+    public String toString() {
+        return user.toString() + "\n" +
+                "Point: (" + this.point.toString() + ")\n" +
+                this.date.toString();
     }
 }
--- src/main/java/GameLogic/Components/MoveList.java
+++ src/main/java/GameLogic/Components/MoveList.java
@@ -3,5 +3,46 @@ package GameLogic.Components;
 import java.util.ArrayList;
 
 public class MoveList {
+    //Should it be some type of stack???
+    private ArrayList<Move> moves; // Array List of moves
 
+
+    // Constructor
+    public MoveList (){
+        moves = new ArrayList<Move>();
+    }
+
+    /**
+     * Adds a move object to the moves ArrayList
+     * @param move - a single Move object a user has made.
+     */
+    public void addMove( Move move) {
+        moves.add( move );
+    }
+
+    /**
+     * Clears the list of moves
+     */
+    public void clearMoves() {
+        moves.clear();
+    }
+
+
+    @Override
+    public String toString() {
+        if( moves.isEmpty() )
+            return "No moves in list";
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < moves.size(); i++) {
+            sb.append( moves.get(i) );
+            sb.append( "\n" );
+        }
+
+        return  sb.toString();
+
+    }
 }
+
+
+
