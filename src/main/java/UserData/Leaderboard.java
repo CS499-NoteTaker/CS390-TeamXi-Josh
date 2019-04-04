e25df77333a5bba5af9fa41d95f5247dbbea2fbc
Kaitlyn Gold <Goldkr@plu.edu>
2019-04-03 17:47:04 -0700

    leaderboard class fix

--- src/main/java/UserData/Leaderboard.java
+++ src/main/java/UserData/Leaderboard.java
@@ -1,33 +1,6 @@
 package UserData;
 import java.util.*;
-
-public class Leaderboard {
-    ArrayList<User> leaderboardList = new ArrayList<>();
-     User[] leaderboard;
-    //get all of the users from user list, get the win ratio from each user, add them to the array list, order
-    //the array list by wins. print that.
-
-    private void createLeaderboard(ArrayList<User> userList){
-        for(int i = 0; i < userList.size(); i++){
-            int win = userList.get(i).getWin();
-            int loss = userList.get(i).getLoss();
-            int mostWinsToLoss = 0;
-            for(int j = 0; j < userList.size(); j++){
-                if(win > loss){
-                    //is it a ratio?
-                    //Associated story:
-                    // As a player, I want to be able to see a leaderboard that shows the top 20 players and their records, so that
-                    //I can compare my progress with others.
-                }
-            }
-        }
-    }
-
-    public  User[] getLeaderboard(){
-        return leaderboard;
-    }
-
-    public static class Leaderboard {
+    public class Leaderboard {
         private ArrayList<User> leaderboard;
 
         public Leaderboard(ArrayList<User> users){
@@ -70,4 +43,4 @@ public class Leaderboard {
             return output;
         }
     }
-}
+
