63a8a0989ae167f021c15100cea31b4fb45272c7
Kaitlyn Gold <goldkr@plu.edu>
2019-04-14 18:08:41 -0700

    example resource

---
+++ src/main/java/Server/Resources/LeaderboardResource.java
@@ -0,0 +1,12 @@
+package Server.Resources;
+
+import com.google.gson.Gson;
+import UserData.Leaderboard;
+
+import javax.inject.Singleton;
+import javax.ws.rs.*;
+import java.util.*;
+
+
+public class LeaderboardResource {
+}
