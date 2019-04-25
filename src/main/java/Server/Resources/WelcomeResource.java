074a04491d7437da2cadf3dee821613a88ee460d
curriese <curriese@plu.edu>
2019-04-24 18:41:51 -0700

    Refactored the starting page and related classes to be consisent

---
+++ src/main/java/Server/Resources/WelcomeResource.java
@@ -0,0 +1,22 @@
+package Server.Resources;
+
+import javax.ws.rs.*;
+import java.io.InputStream;
+
+@Path("welcome")
+public class WelcomeResource {
+
+    @GET
+    public InputStream getClient() {return this.getClass().getResourceAsStream("/welcome.html");}
+
+    @GET
+    @Path("{file}")
+    @Produces("application/javascript")
+    public InputStream getClientJavascript(@PathParam("file") String fileName){
+        InputStream stream = this.getClass().getResourceAsStream("/" + fileName);
+        if(stream == null){
+            throw new WebApplicationException(404);
+        }
+        return stream;
+    }
+}
