04ee249de816417c0d486cc24456b276907db5a8
Joshua <moranja@plu.edu>
2019-04-26 18:49:39 -0700

    Removed if-statements that checks boundaries when creating a game

--- src/main/java/Server/Resources/GameResource.java
+++ src/main/java/Server/Resources/GameResource.java
@@ -74,34 +74,26 @@ public class GameResource {
 
     /**
      * This will help to create game with user1 and user 2
-     * @param user1 - user name from player 1
-     * @param user2 - user name from player 2
+     * @param user1Name - user name from player 1
+     * @param user2Name - user name from player 2
      * @return
      */
+    @PUT
     @Path("{user1}/{user2}/create")
-    public String createGame(@PathParam("user1") String user1, @PathParam("user2") String user2){
+    public int createGame(@PathParam("user1") String user1Name, @PathParam("user2") String user2Name) {
 
-        // Checks for 404 error
-        int id = -1;
-        try {
-            id = Integer.parseInt(GameID);
-        }
-        catch(NumberFormatException e){
-            throw new WebApplicationException(404);
-        }
-
-        // Validate range
-        //todo: change to gameList.getSize()
-        if( id < 0 || id >= Controller.gameList.getSize() ) {
-            throw new WebApplicationException(404);
-        }
+        // Creates two users
+        User user1 = Controller.userList.getUserByUsername( user1Name );
+        User user2 = Controller.userList.getUserByUsername( user2Name );
 
 
+        // Creates new game
+        Game newGame = new Game( user1, user2 );
+        Controller.gameList.addGameToList( newGame );
 
         Gson gson = new Gson();
 
-        //Json
-        return gson.toJson( gson );
+        return 200;
     }
 
 
