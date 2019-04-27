850b0ae233153b3fa6db56e6bb3269d28eff4943
Joshua <moranja@plu.edu>
2019-04-26 18:26:14 -0700

    Adding to path parameters to createGame method for user1 and user2

--- src/main/java/Server/Resources/GameResource.java
+++ src/main/java/Server/Resources/GameResource.java
@@ -38,8 +38,13 @@ public class GameResource {
         return gson.toJson( Controller.gameList.getGameAtIndex(0) );
     }
 
+    /**
+     * This will get the board information in json
+     * @param GameID -  gameIndex
+     * @return - json representation of game[id]
+     */
     @Path("{id}")
-    public String printGameGivenID(@PathParam("id") String GameID){
+    public String getGameInfo(@PathParam("id") String GameID){
 
         // Checks for 404 error
         int id = -1;
@@ -59,6 +64,39 @@ public class GameResource {
 
         //Gets the game at index 'id'
         Game gameId = Controller.gameList.getGameAtIndex(id);
+
+        Gson gson = new Gson();
+
+        //Json
+        return gson.toJson( gson );
+    }
+
+
+    /**
+     * This will help to create game with user1 and user 2
+     * @param user1 - user name from player 1
+     * @param user2 - user name from player 2
+     * @return
+     */
+    @Path("{user1}/{user2}/create")
+    public String createGame(@PathParam("user1") String user1, @PathParam("user2") String user2){
+
+        // Checks for 404 error
+        int id = -1;
+        try {
+            id = Integer.parseInt(GameID);
+        }
+        catch(NumberFormatException e){
+            throw new WebApplicationException(404);
+        }
+
+        // Validate range
+        //todo: change to gameList.getSize()
+        if( id < 0 || id >= Controller.gameList.getSize() ) {
+            throw new WebApplicationException(404);
+        }
+
+
 
         Gson gson = new Gson();
 
--- src/main/java/UserData/Controller.java
+++ src/main/java/UserData/Controller.java
@@ -1,10 +1,20 @@
 package UserData;
 
+import GameLogic.Game;
+
 public class Controller {
 
     public static GameList gameList = new GameList();
     public static UserList userList = new UserList();
 
 
+    public void createGame() {
+        Game tempGame = new Game(new User("Josh"), new User("Daniel"));
+        gameList.addGameToList(tempGame);
+    }
+
+    public void clearGames() {
+
+    }
 
 }
