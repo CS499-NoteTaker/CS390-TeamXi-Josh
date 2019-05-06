48d95c35301e42fe77d28c05496abca24db9e4f0
curriese <curriese@plu.edu>
2019-05-05 18:25:59 -0700

    Added a controllertest class to add tests when the class is fleshed out

---
+++ src/test/java/ControllerTest.java
@@ -0,0 +1,17 @@
+import UserData.Controller;
+import org.junit.Test;
+
+
+//TODO: Add tests if possible
+public class ControllerTest {
+
+    @Test
+    public void TestName01(){
+
+    }
+
+    @Test
+    public void TestName02(){
+
+    }
+}
