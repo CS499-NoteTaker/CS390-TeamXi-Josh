7b89213efbde329fb6ca084a877114cafa4fed68
Joshua <moranja@plu.edu>
2019-05-08 15:09:36 -0700

    Included a coordinate class to convert from json to an object. able to retrieve x-y coordinates

--- src/main/java/Server/Resources/CurrentGameResource.java
+++ src/main/java/Server/Resources/CurrentGameResource.java
@@ -81,13 +81,13 @@ public class CurrentGameResource {
 
 
 
-    @PUT
+    @POST
     @Path("{id}/placePoint")
-    public String placePointOnBoard(@PathParam("id") String GameId ) {
+    public String placePointOnBoard(@PathParam("id") String gameId, String coordinateData ) {
         // Convert to an integer
         int id = -1;
         try {
-            id = Integer.parseInt( GameId );
+            id = Integer.parseInt( gameId );
         } catch( NumberFormatException e ) {
             throw new WebApplicationException(404);
         }
@@ -97,12 +97,61 @@ public class CurrentGameResource {
             throw new WebApplicationException(404);
         }
 
+        // Parse the data from the client
+        JsonObject jsonObj = new JsonObject();
+        int xCord = Integer.parseInt( String.valueOf(jsonObj.get("x")) );
 
-        return "";
+
+        //Todo: make sure to track the moves into the move list as well
+        Piece piece;
+        Coordinate coordinate;
+        User player1, player2, currentUser;
+        Game currentGame;
+
+        // Converts the x and y coordinates from json to Coordinate object
+        Gson gson = new Gson();
+        coordinate = gson.fromJson(coordinateData, Coordinate.class);
+
+        // Grabs necessary game data
+        currentUser = WelcomeResource.currentUser;
+        currentGame = Controller.gameList.getGameAtIndex( id );
+        player1 = currentGame.getUser1();
+        player2 = currentGame.getUser2();
+
+
+        // Checks to see which player the current player is within the game
+        // so that a piece can be layed down accordingly
+        if( currentUser.usernameEquals( player1.getUserName() ) ) {
+            piece = Piece.B;
+        } else if( currentUser.usernameEquals( player2.getUserName()) ) {
+            piece = Piece.W;
+        } else {
+            piece = Piece.O;
+            System.out.println(" Not current user is not player1 or player2");
+        }
+
+        currentGame.gameBoard.setPointAtLocation(coordinate.x, coordinate.y, piece);
+
+        return "200";
     }
 
 
 
+    class Coordinate {
+        int x;
+        int y;
+
+        public Coordinate( int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() { return this.x; }
+        public int getY() { return this.y; }
+
+        public void setX(int x) { this.x = x; }
+        public void setY(int y) { this.y = y; }
+    }
 
 
 }
